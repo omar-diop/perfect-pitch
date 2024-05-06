40789fc2e351c16d5bb3a52dbd84ff8c821b145e
Omar Diop <accounts@omardiop.com>
2024-05-06 16:44:25 +0200

    feat: added GTM

---
+++ src/app/components/TagManager/TagManager.tsx
@@ -0,0 +1,15 @@
+import Script from "next/script"
+
+export function TagManager() {
+  return (
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KQMH9JXF');
+  `}
+    </Script>
+  )
+}
